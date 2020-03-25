0216a76678cddda6c0201c213894d1c90610f775
Shawn DelPercio <sdelperc@gmail.com>
2020-03-25 16:30:59 -0400

    added basic tests for repair and fail funtions

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,64 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+it('should run the test', () => {
+	expect(true).toBe(true);
+});
+
+describe('enhancer', () => {
+	describe('repair function', () => {
+		it('should repair an items durability to 100 if durability is less than 100', () => {
+			const item = {
+				name: 'Mjolnir',
+				durability: 50,
+				enhancement: 12
+			};
+			const itemOutput = {
+				name: 'Mjolnir',
+				durability: 100,
+				enhancement: 12
+			};
+			expect(enhancer.repair(item)).toStrictEqual(itemOutput);
+		});
+	});
+	describe('enhance fail function', () => {
+		it('should reduce durability by 5 if enhancement is < 15', () => {
+			const item = {
+				name: 'Mjolnir',
+				durability: 50,
+				enhancement: 12
+			};
+			const itemOutput = {
+				name: 'Mjolnir',
+				durability: 45,
+				enhancement: 12
+			};
+			expect(enhancer.fail(item)).toStrictEqual(itemOutput);
+		});
+		it('should reduce durability by 10, and not reduce enhancement level if item enhancement is 15 or 16', () => {
+			const item = {
+				name: 'Mjolnir',
+				durability: 50,
+				enhancement: 16
+			};
+			const itemOutput = {
+				name: 'Mjolnir',
+				durability: 40,
+				enhancement: 16
+			};
+			expect(enhancer.fail(item)).toStrictEqual(itemOutput);
+		});
+		it('should reduce durability by 10 and reduce enhancement level by 1 if item enhancement > 16', () => {
+			const item = {
+				name: 'Mjolnir',
+				durability: 50,
+				enhancement: 18
+			};
+			const itemOutput = {
+				name: 'Mjolnir',
+				durability: 40,
+				enhancement: 17
+			};
+			expect(enhancer.fail(item)).toStrictEqual(itemOutput);
+		});
+	});
+});
