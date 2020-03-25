1011afabb9f40a690fa9f8d534fa3ac7342f5a08
Shawn DelPercio <sdelperc@gmail.com>
2020-03-25 15:54:55 -0400

    implemented succeed and fail functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
-	return { ...item };
+	if (item.enhancement >= 20) {
+		return { ...item };
+	} else {
+		item.enhancement += 1;
+		return { ...item };
+	}
 }
 
 function fail(item) {
-	return { ...item };
+	if (item.enhancement < 15) {
+		item.durability -= 5;
+		return { ...item };
+	} else if (item.enhancement === 15 || item.enhancement === 16) {
+		item.durability -= 10;
+		return { ...item };
+	} else if (item.enhancement > 16) {
+		item.durability -= 10;
+		item.enhancement -= 1;
+		return { ...item };
+	}
 }
 
 function repair(item) {
