235aa6fc9c5d2b43fe93488f86c7e1f358531e97
Shawn DelPercio <sdelperc@gmail.com>
2020-03-25 15:28:55 -0400

    added jest, implemented repair function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,28 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+	if (item) {
+		item.durability = 100;
+
+		return { ...item };
+	} else {
+		throw new Error('please add your item to repair');
+	}
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
